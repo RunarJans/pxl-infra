6ed1d84bb25d9bfb4e34d8e55813b18483dd0d4a
Bryan Honof <bryan.honof@pxl.be>
2024-07-23 21:20:36 +0200

    chore: Disable Bowen's server

--- servers.tf
+++ servers.tf
@@ -26,16 +26,16 @@ locals {
         "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDAq0D+rESyOGnAfqEjyWo+Qe4JNJp3rwBOYasEm0h6Tj3Z7yrWtF16bIApuR9l/crShtui0cf9WtzgCmhPmI4HhoIIv5nVo8ERU1E/QdG27JOGSypLusds7PgAyiRLUqufBpi7hD3C5R/xxerllNDvXFC/k62sIkPYeEkkKsUsiXKAvFrA8723O7Z3fNM1/EFZ2hhw+VKhYq0vQ8OVVZQ/VFOb1yeJJxXCzuslfPRx1I67YCTvnwuPSf93yvO+1Q/lPoWZsNf7okOLHZxjmaRAty6eXwHCtk7PiIjCjU++RdJtaQPiNzoI3S17dvAgFnCQYW09unNePfGbJQTvHLCFlKJQA9AE2uL5SEHyAp9ND5SMUK+zJEn59pDRzxX/j3SD6lx3iS1vy25NPQc6XUAuGUMg70lAQlsgAAWrUH3yRnG2CNA9m3OG9xl+SFrR4vkAgZodtspqJEUTo+rXE+0Q31+WNUfUMN69OspKmizLcJtyMNSDjfr056OP/cVIkoU= bjarni heselmans@5CD322B28R",
       ]
     }
-    Bowen = {
-      ssh_keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHZuoIwLhSLkChxOWJZqb+YpLMoE1ql0OHrLCh2fq14O bowen.liang@student.pxl.be",
-      ]
-    }
     Anish = {
       ssh_keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIrvcSz2EnQieZyeUOm7aiQN+WCng1uH8fp2Nt/BrAuo Anish.Pun@student.pxl.be",
       ]
     }
+    # Bowen = {
+    #   ssh_keys = [
+    #     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHZuoIwLhSLkChxOWJZqb+YpLMoE1ql0OHrLCh2fq14O bowen.liang@student.pxl.be",
+    #   ]
+    # }
   }
 }
 
